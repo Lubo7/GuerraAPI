a5e6284331c3bc06052770ad221ecd1b9fb62c89
madjek <90720831+madjek@users.noreply.github.com>
2021-10-22 13:44:23 +0200

    index port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const morgan = require('morgan');
 const logger = require('./config/winston');
 
 const app = express();
-const PORT = 3000;
+let PORT = process.env.YOUR_PORT || process.env.PORT || 3000;
 
 //Middleware
 app.use(morgan('combined', { stream: logger.stream }));
